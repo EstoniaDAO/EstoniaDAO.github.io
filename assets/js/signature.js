a1f23fb978896c461827acb8646dfce105175b8a
M <>
2020-04-20 03:09:42 +0100

    Cleanup

--- assets/js/signature.js
+++ assets/js/signature.js
@@ -1,5 +1,7 @@
 var app = angular.module("app", ["ngRoute"]);
 
+const ipfs = window.IpfsHttpClient('ipfs.infura.io', '5001', { protocol: 'https' });
+
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
@@ -12,133 +14,111 @@ app.config(function ($routeProvider) {
     .otherwise('/')
 });
 
-app.controller("DemoCtrl", function($scope) {
-  $scope.message = "It works!";
+app.controller("DemoCtrl", function($scope, $http, $q) {
+
+  $scope.connect = async function() {
+    ethEnabled();
+    accounts = await web3.eth.getAccounts()
+    $scope.address = accounts[0];
+    $scope.$apply();
+  }
+
+  $scope.sign = async function() {
+    signature = await web3.eth.personal.sign($scope.address, accounts[0]);
+    $scope.signature = signature;
+    $scope.$apply();
+  }
+
+  $scope.download = function() {
+    if (!$scope.account || !$scope.signature) {
+      alert("Please ensure both address and signature are present.")
+      return;
+    }
+    var data = {
+      "address": $scope.account,
+      "signature": $scope.signature
+    }
+    saveData(data, "wallet.json");
+  }
+
+  $scope.uploadFile = function(event){
+    var file = event.target.files[0]
+
+    let fileReader = new FileReader();
+    fileReader.onload = function(e) {
+
+      const magic = buffer.Buffer(fileReader.result); // honestly as a web developer I do not fully appreciate the difference between buffer and arrayBuffer 
+      ipfs.add(magic, (err, result) => {
+        console.log(err, result);
+        $scope.url = "https://gateway.ipfs.io/ipfs/" + result[0].hash;
+        $scope.$apply();
+
+        console.log($scope.url);
+
+        makeRequest($scope.url).then(function(result) {
+          if (result) {
+            console.log("SIGNATURE VALID");
+          }
+        })
+
+      });
+    };
+    fileReader.readAsArrayBuffer(file);
+
+  };
+
+
+
+  let makeRequest = function(url) {
+    let defer = $q.defer();
+
+    // Full good file: https://polished-silence-366.fly.dev/verify?url=https://gateway.pinata.cloud/ipfs/QmVFST2tbVR6bokaMYXPxJJQXELwLfqWSrRaBgZqwaQT7Y/wallet.asicev
+    let baseURL = "https://polished-silence-366.fly.dev/verify?url=";
+
+    $http({
+      method: 'GET',
+      url: baseURL + url
+    }).then(function successCallback(response) {
+        let responseData = response.data;
+        let signatureValid = responseData.indexOf("Signature method" !== -1) && responseData.indexOf("Signing time" !== -1) && responseData.indexOf("Signing cert" !== -1) && responseData.indexOf("Signed by" !== -1)
+
+        defer.resolve(signatureValid);
+
+      }, function errorCallback(response) {
+        console.error(response.data);
+        defer.reject(response.data)
+
+      });
+
+      return defer.promise;
+  }
+
+
+
+
+
 });
 
 
 
-// const ipfs = window.IpfsHttpClient('ipfs.infura.io', '5001', { protocol: 'https' });
-
-
-// // DRAG AND DROP
-// // https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
-// let dropArea = document.getElementById("drop-area")
-
-// // Prevent default drag behaviors
-// ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-//   dropArea.addEventListener(eventName, preventDefaults, false)   
-//   document.body.addEventListener(eventName, preventDefaults, false)
-// })
-
-// // Highlight drop area when item is dragged over it
-// ;['dragenter', 'dragover'].forEach(eventName => {
-//   dropArea.addEventListener(eventName, highlight, false)
-// })
-
-// ;['dragleave', 'drop'].forEach(eventName => {
-//   dropArea.addEventListener(eventName, unhighlight, false)
-// })
-
-// // Handle dropped files
-// dropArea.addEventListener('drop', handleDrop, false)
-
-// function preventDefaults (e) {
-//   e.preventDefault()
-//   e.stopPropagation()
-// }
-
-// function highlight(e) {
-//   dropArea.classList.add('highlight')
-// }
-
-// function unhighlight(e) {
-//   dropArea.classList.remove('highlight')
-// }
-
-// // https://stackoverflow.com/questions/40031688/javascript-arraybuffer-to-hex
-// function buf2hex(buffer) { // buffer is an ArrayBuffer
-//   return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-// }
-
-// $("#macaroon").on("change", handleDrop);
 
 
 
-function handleDrop(e) {
-  let file;
-  if (e.type === "change") {
-    file = e.target.files[0]
-  } else if (e.type === "drop") {
-    file = e.dataTransfer.files[0]; 
-  }
-  
-  if (file) {
-    let fileReader = new FileReader();
-    fileReader.onload = function(e) {
-      // let macaroon = buf2hex(e.target.result);
-      // $("#macaroon-textarea").val(macaroon);
-      // chrome.storage.sync.set({ macaroon: macaroon }, function() {
-      //   console.log("storage updated macaroon", macaroon);
-      // });
-    
-      const magic = buffer.Buffer(fileReader.result); // honestly as a web developer I do not fully appreciate the difference between buffer and arrayBuffer 
-      ipfs.add(magic, (err, result) => {
-        console.log(err, result);
-        url = "https://gateway.ipfs.io/ipfs/" + result[0].hash;
-        let ipfsLink = "<a href='https://gateway.ipfs.io/ipfs/" + result[0].hash + "'>gateway.ipfs.io/ipfs/" + result[0].hash + "</a>";
-        document.getElementById("link").innerHTML = ipfsLink;
+// https://stackoverflow.com/a/19647381/775359
+app.directive('customOnChange', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var onChangeHandler = scope.$eval(attrs.customOnChange);
+      element.on('change', onChangeHandler);
+      element.on('$destroy', function() {
+        element.off();
       });
-    };
-    fileReader.readAsArrayBuffer(file);
-  }
-}
 
+    }
+  };
+});
 
-$("#connect").on("click", async function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-
-  ethEnabled();
-  accounts = await web3.eth.getAccounts()
-  $("#ethaddress").val(accounts[0])
-
-
-})
-
-$("#sign").on("click", async function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-
-  signature = await web3.eth.personal.sign(accounts[0], accounts[0]); // signing the message "0x85" from the account
-  $("#signature").val(signature)
-
-  return false;
-})
-
-$("#download").on("click", async function() {
-  event.preventDefault();
-  event.stopPropagation();
-
-  var data = {
-    "address": accounts[0],
-    "signature": signature
-  }
-  saveData(data, "wallet.json");
-
-  return false;
-})
-
-$("#sendgettodev").on("click", async function() {
-
-  $.get("https://polished-silence-366.fly.dev/verify?url=https://gateway.pinata.cloud/ipfs/QmVFST2tbVR6bokaMYXPxJJQXELwLfqWSrRaBgZqwaQT7Y/wallet.asice", function(data) {
-    console.log(data);
-  });
-
-})
-
-
-$("#sendwebhook").on("click", sendMessage)
 
 
 
@@ -151,25 +131,32 @@ const ethEnabled = () => {
   return false;
 }
 
-
-
-
-
 var saveData = (function () {
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    return function (data, fileName) {
-        var json = JSON.stringify(data),
-            blob = new Blob([json], {type: "octet/stream"}),
-            url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = fileName;
-        a.click();
-        window.URL.revokeObjectURL(url);
-    };
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  return function (data, fileName) {
+      var json = JSON.stringify(data),
+          blob = new Blob([json], {type: "octet/stream"}),
+          url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = fileName;
+      a.click();
+      window.URL.revokeObjectURL(url);
+  };
 }());
 
+
+$("#sendgettodev").on("click", async function() {
+
+  $.get("https://polished-silence-366.fly.dev/verify?url=https://gateway.pinata.cloud/ipfs/QmVFST2tbVR6bokaMYXPxJJQXELwLfqWSrRaBgZqwaQT7Y/wallet.asice", function(data) {
+    console.log(data);
+  });
+
+})
+
+
+$("#sendwebhook").on("click", sendMessage)
 
 function sendMessage() {
   var request = new XMLHttpRequest();
@@ -185,36 +172,3 @@ function sendMessage() {
 
   request.send(JSON.stringify(params));
 }
-
-
-
-
-// isEnabled = ethEnabled();
-// console.log(isEnabled);
-
-
-
-  // try {
-  //   accounts = ethereum.enable();
-  // } catch (error) {
-  //   console.log(error);
-  // }
-
-  // provider = new ethers.providers.Web3Provider(web3.currentProvider);
-
-  // network = provider.getNetwork();
-
-  // console.log(network);
-
-  // if (network.chainId !== 42) {
-  //   gotofail();
-  // }
-
-  // let networkLongPolling = setInterval(async function(){
-  //   network = await provider.getNetwork()
-
-  //   if (network.chainId !== 42) {
-  //     clearInterval(networkLongPolling);
-  //     gotofail();
-  //   }
-  // }, 1000);
